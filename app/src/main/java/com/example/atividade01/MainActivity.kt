80275a3754ac363f1765ddfe201556f9f38ba06a
Pedro Farias <phjfarias@hotmail.com>
2021-05-17 00:01:42 -0300

    feat: create files with jetpack

--- app/src/main/java/com/example/atividade01/MainActivity.kt
+++ app/src/main/java/com/example/atividade01/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.atividade01
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.security.crypto.EncryptedFile
+import androidx.security.crypto.MasterKeys
 import com.example.atividade01.adapter.FileAdapter
 import com.example.atividade01.data.FileData
 import kotlinx.android.synthetic.main.activity_main.*
@@ -34,45 +37,43 @@ open class MainActivity : AppCompatActivity(), FileAdapter.OnItemClickListener {
             val fileName = file_name_input.text.toString()
             val fileContents = file_content_input.text.toString()
 
+            // Check if the fileName isn't null
             if (fileName.isEmpty()) {
                 Toast.makeText(this, "Provide at least the file name", Toast.LENGTH_SHORT).show()
             } else {
+
+                // Check if will create an internal or external file
                 if (isInternal) {
-                    try {
-                        this.openFileOutput(fileName, MODE_PRIVATE).use {
-                            it.write(fileContents.toByteArray())
+
+                    // Check if will use jetpack or not
+                    if (!useJetpack.isChecked) {
+                        this.createInternalFile(fileName, fileContents)
+                    } else {
+                        val file = File(filesDir, fileName)
+
+                        if (file.exists()) {
+                            file.delete()
                         }
 
-                        Toast.makeText(this, "File created", Toast.LENGTH_SHORT).show()
-                    } catch (e: Exception) {
-                        e.printStackTrace()
+                        createEncryptedFile(file, fileContents)
                     }
+
                 } else {
-                    val file = File(this.getExternalFilesDir(null), fileName)
-                    val fileOutputStream = FileOutputStream(file)
 
-                    try {
-                        fileOutputStream.use { stream ->
-                            stream.write(fileContents.toByteArray())
+                    // Check if will use jetpack or not
+                    if (!useJetpack.isChecked) {
+                        this.createExternalFile(fileName, fileContents)
+                    } else {
+                        val file = File(getExternalFilesDir(null), fileName)
+
+                        if (file.exists()) {
+                            file.delete()
                         }
 
-                        Toast.makeText(
-                            this,
-                            "File created ${this.getExternalFilesDir(null)}",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    } catch (e: Exception) {
-                        e.printStackTrace()
+                        createEncryptedFile(file, fileContents)
                     }
-
                 }
             }
-
-            val files: Array<String> = this.fileList()
-
-            println("DEBUG FILES: $files")
-
-
         }
 
     }
@@ -90,6 +91,54 @@ open class MainActivity : AppCompatActivity(), FileAdapter.OnItemClickListener {
         }
 
         return list
+    }
+
+    private fun createEncryptedFile(file: File, content: String) {
+        val context: Context = applicationContext
+        val keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC
+        val masterKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec)
+
+        val encryptedFile = EncryptedFile.Builder(
+            file,
+            context,
+            masterKeyAlias,
+            EncryptedFile.FileEncryptionScheme.AES256_GCM_HKDF_4KB
+        ).build()
+
+        encryptedFile.openFileOutput().bufferedWriter().use { bufferedWriter ->
+            bufferedWriter.write(content)
+        }
+    }
+
+    private fun createInternalFile(fileName: String, fileContents: String) {
+        try {
+            this.openFileOutput(fileName, MODE_PRIVATE).use {
+                it.write(fileContents.toByteArray())
+            }
+
+            Toast.makeText(this, "File created", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun createExternalFile(fileName: String, fileContents: String) {
+        val file = File(this.getExternalFilesDir(null), fileName)
+        val fileOutputStream = FileOutputStream(file)
+
+        try {
+            fileOutputStream.use { stream ->
+                stream.write(fileContents.toByteArray())
+            }
+
+            Toast.makeText(
+                this,
+                "File created ${this.getExternalFilesDir(null)}",
+                Toast.LENGTH_SHORT
+            ).show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     override fun onItemClick(position: Int) {
